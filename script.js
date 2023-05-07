46db1b0154124eb7e1ddd073c3644d4b879b2df8
Felipefar <111538929+Felipefar@users.noreply.github.com>
2023-05-07 09:42:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const signUpButton = document.getElementById('signUp')
 const signInButton = document.getElementById('signIn')
 const container = document.getElementById('container')
 
-signUpButton.addEventListener('click', ()=> {
-    container.classList.add("right-painel-active")
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-painel-active')
 })
 
-signInButton.addEventListener('click', ()=> {
-    container.classList.remove("right-painel-active")
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-painel-active')
 })
